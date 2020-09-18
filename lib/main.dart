42e22ca4b2768199a968ed350b30c275491a4218
iuzul <iuzul@IBRAHIMUZUL>
2020-09-18 13:33:17 +0300

    FloatingActionButton ve Material Design Kuralları

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,34 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(new MaterialApp(
     debugShowCheckedModeBanner: false,
-    theme: ThemeData(
-      primarySwatch: Colors.cyan,
-    ),
+    theme: ThemeData(primarySwatch: Colors.cyan, accentColor: Colors.orange),
     home: new Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Flutter Dersleri",
-          style: TextStyle(fontSize: 24.0, color: Colors.white),
+        appBar: AppBar(
+          title: Text(
+            "Flutter Dersleri",
+            style: TextStyle(fontSize: 24.0, color: Colors.white),
+          ),
         ),
-      ),
-    ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            debugPrint("FAB tıklandı");
+          },
+          //backgroundColor: Colors.green,
+          child: Icon(
+            Icons.access_alarms,
+            color: Colors.white,
+            //size: 48,
+          ),
+        ),
+        //floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        body: Center(
+          child: Container(
+            //color:Colors.purple,
+            child: Text(
+              "Flutter Harika",
+              textAlign: TextAlign.center,
+            ),
+          ),
+        )),
   ));
 }
