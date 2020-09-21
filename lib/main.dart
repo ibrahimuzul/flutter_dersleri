f09c175209d7c3faa20e30b2d0103d13926a748a
iuzul <iuzul@IBRAHIMUZUL>
2020-09-21 13:20:04 +0300

    56. Kendi denemem

--- lib/main.dart
+++ lib/main.dart
@@ -25,15 +25,19 @@ void main() {
       //floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       body: Column(
         mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
+          Container(
+            alignment: Alignment.center,
+            child: Text("Image ve Buton Türleri",textAlign: TextAlign.center,style:TextStyle(fontSize: 24),),
+          ),
           Row(
             mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               Container(
-                padding: EdgeInsets.all(20),
+                padding: EdgeInsets.all(40),
                 margin: EdgeInsets.all(2),
                 color: Colors.red.shade100,
                 child: Text(
@@ -42,96 +46,57 @@ void main() {
                 ),
               ),
               Container(
-                padding: EdgeInsets.all(20),
+                padding: EdgeInsets.all(40),
                 margin: EdgeInsets.all(2),
-                color: Colors.red.shade300,
+                color: Colors.red.shade100,
                 child: Text(
                   "A",
                   style: TextStyle(fontSize: 24),
                 ),
               ),
               Container(
-                padding: EdgeInsets.all(20),
+                padding: EdgeInsets.all(40),
                 margin: EdgeInsets.all(2),
-                color: Colors.red.shade500,
+                color: Colors.red.shade100,
                 child: Text(
                   "R",
                   style: TextStyle(fontSize: 24),
                 ),
               ),
+            ],
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
               Container(
-                padding: EdgeInsets.all(20),
+                padding: EdgeInsets.all(40),
                 margin: EdgeInsets.all(2),
-                color: Colors.red.shade700,
+                color: Colors.red.shade100,
                 child: Text(
-                  "T",
+                  "D",
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(40),
+                margin: EdgeInsets.all(2),
+                color: Colors.red.shade100,
+                child: Text(
+                  "A",
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(40),
+                margin: EdgeInsets.all(2),
+                color: Colors.red.shade100,
+                child: Text(
+                  "R",
                   style: TextStyle(fontSize: 24),
                 ),
               ),
             ],
-          ),
-          Container(
-            padding: EdgeInsets.all(20),
-            margin: EdgeInsets.all(2),
-            color: Colors.red.shade200,
-            child: Text(
-              "E",
-              style: TextStyle(fontSize: 24),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(20),
-            margin: EdgeInsets.all(2),
-            color: Colors.red.shade300,
-            child: Text(
-              "R",
-              style: TextStyle(fontSize: 24),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(20),
-            margin: EdgeInsets.all(2),
-            color: Colors.red.shade400,
-            child: Text(
-              "S",
-              style: TextStyle(fontSize: 24),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(20),
-            margin: EdgeInsets.all(2),
-            color: Colors.red.shade500,
-            child: Text(
-              "L",
-              style: TextStyle(fontSize: 24),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(20),
-            margin: EdgeInsets.all(2),
-            color: Colors.red.shade600,
-            child: Text(
-              "E",
-              style: TextStyle(fontSize: 24),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(20),
-            margin: EdgeInsets.all(2),
-            color: Colors.red.shade700,
-            child: Text(
-              "R",
-              style: TextStyle(fontSize: 24),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(26, 22, 22, 22),
-            margin: EdgeInsets.all(2),
-            color: Colors.red.shade800,
-            child: Text(
-              "İ",
-              style: TextStyle(fontSize: 24),
-            ),
           ),
         ],
       ),
