6f08fb9935e3cf84e5d8e41638b4c43fc44d75e5
iuzul <iuzul@IBRAHIMUZUL>
2020-09-21 11:18:41 +0300

    55. Ödev Çözümü

--- lib/main.dart
+++ lib/main.dart
@@ -23,153 +23,117 @@ void main() {
         ),
       ),
       //floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      body: Container(
-        color: Colors.white,
-        child: Row(
-          //child: Column(
-          mainAxisSize: MainAxisSize.min,
-          //mainAxisSize: MainAxisSize.min,
-          //mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            Expanded(
-              child: Container(
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    Expanded(
-                      child: Container(
-                        child: Icon(
-                          Icons.add_circle,
-                          size: 64,
-                          color: Colors.yellow,
-                        ),
-                        color: Colors.blue.shade200,
-                      ),
-                      flex: 2,
-                    ),
-                    Expanded(
-                      child: Container(
-                        child: Icon(
-                          Icons.add_circle,
-                          size: 64,
-                          color: Colors.yellow,
-                        ),
-                        color: Colors.blue.shade200,
-                      ),
-                      flex: 2,
-                    ),
-                    Expanded(
-                      child: Container(
-                        child: Icon(
-                          Icons.add_circle,
-                          size: 64,
-                          color: Colors.yellow,
-                        ),
-                        color: Colors.blue.shade200,
-                      ),
-                      flex: 2,
-                    ),
-                    Expanded(
-                      child: Container(
-                        child: Icon(
-                          Icons.add_circle,
-                          size: 64,
-                          color: Colors.yellow,
-                        ),
-                        color: Colors.blue.shade200,
-                      ),
-                      flex: 2,
-                    ),
-                    Expanded(
-                      child: Container(
-                        child: Icon(
-                          Icons.add_circle,
-                          size: 64,
-                          color: Colors.yellow,
-                        ),
-                        color: Colors.blue.shade200,
-                      ),
-                      flex: 2,
-                    ),
-                    Expanded(
-                      child: Container(
-                        child: Icon(
-                          Icons.add_circle,
-                          size: 64,
-                          color: Colors.yellow,
-                        ),
-                        color: Colors.blue.shade200,
-                      ),
-                      flex: 2,
-                    ),
-                    Expanded(
-                      child: Container(
-                        child: Icon(
-                          Icons.add_circle,
-                          size: 64,
-                          color: Colors.yellow,
-                        ),
-                        color: Colors.blue.shade200,
-                      ),
-                      flex: 2,
-                    ),
-                    Expanded(
-                      child: Container(
-                        child: Icon(
-                          Icons.add_circle,
-                          size: 64,
-                          color: Colors.yellow,
-                        ),
-                        color: Colors.blue.shade200,
-                      ),
-                      flex: 2,
-                    ),
-                  ],
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(2),
+                color: Colors.red.shade100,
+                child: Text(
+                  "D",
+                  style: TextStyle(fontSize: 24),
                 ),
               ),
-              flex: 2,
-            ),
-
-            Expanded(
-              child: Container(
-                child: Icon(
-                  Icons.add_circle,
-                  size: 64,
-                  color: Colors.yellow,
+              Container(
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(2),
+                color: Colors.red.shade300,
+                child: Text(
+                  "A",
+                  style: TextStyle(fontSize: 24),
                 ),
-                color: Colors.green.shade200,
               ),
-              flex: 2,
-            ),
-            Expanded(
-              child: Container(
-                child: Icon(
-                  Icons.add_circle,
-                  size: 64,
-                  color: Colors.yellow,
+              Container(
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(2),
+                color: Colors.red.shade500,
+                child: Text(
+                  "R",
+                  style: TextStyle(fontSize: 24),
                 ),
-                color: Colors.purple.shade200,
               ),
-              flex: 2,
-            ),
-            Expanded(
-              child: Container(
-                child: Icon(
-                  Icons.add_circle,
-                  size: 64,
-                  color: Colors.yellow,
+              Container(
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(2),
+                color: Colors.red.shade700,
+                child: Text(
+                  "T",
+                  style: TextStyle(fontSize: 24),
                 ),
-                color: Colors.orange.shade200,
               ),
-              flex: 2,
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.all(2),
+            color: Colors.red.shade200,
+            child: Text(
+              "E",
+              style: TextStyle(fontSize: 24),
             ),
-          ],
-        ),
+          ),
+          Container(
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.all(2),
+            color: Colors.red.shade300,
+            child: Text(
+              "R",
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.all(2),
+            color: Colors.red.shade400,
+            child: Text(
+              "S",
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.all(2),
+            color: Colors.red.shade500,
+            child: Text(
+              "L",
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.all(2),
+            color: Colors.red.shade600,
+            child: Text(
+              "E",
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.all(2),
+            color: Colors.red.shade700,
+            child: Text(
+              "R",
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(26, 22, 22, 22),
+            margin: EdgeInsets.all(2),
+            color: Colors.red.shade800,
+            child: Text(
+              "İ",
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+        ],
       ),
     ),
   ));
