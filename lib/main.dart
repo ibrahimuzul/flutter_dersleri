71a2d829fa579cf8b374b35e855727bba57d371e
iuzul <iuzul@IBRAHIMUZUL>
2020-09-21 17:40:52 +0300

    62. Stateful Widgeti Anlayalım Bölüm 2

--- lib/main.dart
+++ lib/main.dart
@@ -12,36 +12,70 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       title: "MaterialApp Title",
-      home: MyHomePage(title:"My Home Page"),
+      home: MyHomePage(title: "My Home Page"),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-
+class MyHomePage extends StatefulWidget {
   final String title;
+  int sayac = 0;
 
-  MyHomePage({this.title});
+  MyHomePage({this.title}){
+    debugPrint("MyhomePage Statefull widget constructor çalıştırıldı");
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    debugPrint("MyhomePage Statefull widget createState çalıştırıldı");
+    return MyHomePageState();
+  }
+}
+
+class MyHomePageState extends State<MyHomePage> {
+
+  MyHomePageState()
+  {
+    debugPrint("MyHomePageState State  constructor çalıştırıldı");
+  }
 
   @override
   Widget build(BuildContext context) {
+    debugPrint("MyHomePageState build metodu tetkiklendi");
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text("${widget.title}"),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          setState(() {
+            sayacDegeriniArttir();
+            debugPrint("sayac değeri arttırıldı set state çağırıldı");
+          });
+
+        },
         child: Icon(Icons.add),
       ),
-        body: Center(
-          child:Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Butona Basılma Sayısı"),
-              Text("5",style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold,),)
-            ],
-          ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Butona Basılma Sayısı"),
+            Text(
+              "${widget.sayac}",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            )
+          ],
         ),
+      ),
     );
+  }
+
+  void sayacDegeriniArttir() {
+    widget.sayac++;
+    debugPrint(widget.sayac.toString());
   }
 }
