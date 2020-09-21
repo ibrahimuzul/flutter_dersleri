aad725a6b0f55f6dd0c001d54245d86f9e1c3ebf
iuzul <iuzul@IBRAHIMUZUL>
2020-09-21 14:54:23 +0300

    57. FadeinImage ve InstrinsicHeight Kullanımı

--- lib/main.dart
+++ lib/main.dart
@@ -30,55 +30,84 @@ void main() {
               "Resim ve Buton Türleri",
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.red.shade200,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      //Image.asset("assets/images/icon.png"),
-                      Image.network("http://hdqwalls.com/wallpapers/small-memory-lp.jpg"),
-                      Text("Asset Image"),
-                    ],
-                  ),
-                ),
-
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.red.shade200,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Image.network("http://hdqwalls.com/wallpapers/small-memory-lp.jpg"),
-                      Text("Network Image"),
-                    ],
-                  ),
-                ),
-
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.red.shade200,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      CircleAvatar(
-                        backgroundImage: NetworkImage("https://emrealtunbilek.com/wp-content/uploads/2016/10/apple-icon-72x72.png"),
-                        backgroundColor: Colors.purple,
-                        radius: 30,
-                        foregroundColor: Colors.orange,
+            IntrinsicHeight(
+              child:Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(4),
+                      color: Colors.red.shade200,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          //Image.asset("assets/images/icon.png"),
+                          Image.network(
+                              "http://hdqwalls.com/wallpapers/small-memory-lp.jpg"),
+                          Text("Asset Image"),
+                        ],
                       ),
-                      Text("Circle Avatar"),
-                    ],
+                    ),
                   ),
-                ),
-              ],
-            )
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(4),
+                      color: Colors.red.shade200,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Image.network(
+                              "http://hdqwalls.com/wallpapers/small-memory-lp.jpg"),
+                          Text("Network Image"),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(4),
+                      color: Colors.red.shade200,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                "https://emrealtunbilek.com/wp-content/uploads/2016/10/apple-icon-72x72.png"),
+                            backgroundColor: Colors.purple,
+                            radius: 30,
+                            foregroundColor: Colors.orange,
+                          ),
+                          Text("Circle Avatar"),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ),
+            IntrinsicHeight(
+                child:Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.all(4),
+                        color: Colors.red.shade200,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            //Image.asset("assets/images/icon.png"),
+                            //Image.network("http://hdqwalls.com/wallpapers/small-memory-lp.jpg"),
+                            FadeInImage.assetNetwork(placeholder: "assets/images/loading.gif", image: "http://hdqwalls.com/wallpapers/small-memory-lp.jpg",),
+                            Text("Fade In Image"),
+                          ],
+                        ),
+                      ),
+                    ),
+                    
+                  ],
+                )
+            ),
           ],
         ),
       ),
