009a3354637e1364b72adb1b7d79daf2315afb09
iuzul <iuzul@IBRAHIMUZUL>
2020-09-21 15:23:56 +0300

    58. FlutterLogo ve PlaceHolder Widget özellikleri

--- lib/main.dart
+++ lib/main.dart
@@ -31,83 +31,120 @@ void main() {
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
             IntrinsicHeight(
-              child:Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(4),
-                      color: Colors.red.shade200,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          //Image.asset("assets/images/icon.png"),
-                          Image.network(
-                              "http://hdqwalls.com/wallpapers/small-memory-lp.jpg"),
-                          Text("Asset Image"),
-                        ],
-                      ),
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(4),
+                    color: Colors.red.shade200,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        //Image.asset("assets/images/icon.png"),
+                        Image.network(
+                            "http://hdqwalls.com/wallpapers/small-memory-lp.jpg"),
+                        Text("Asset Image"),
+                      ],
                     ),
                   ),
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(4),
-                      color: Colors.red.shade200,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Image.network(
-                              "http://hdqwalls.com/wallpapers/small-memory-lp.jpg"),
-                          Text("Network Image"),
-                        ],
-                      ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(4),
+                    color: Colors.red.shade200,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Image.network(
+                            "http://hdqwalls.com/wallpapers/small-memory-lp.jpg"),
+                        Text("Network Image"),
+                      ],
                     ),
                   ),
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(4),
-                      color: Colors.red.shade200,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          CircleAvatar(
-                            backgroundImage: NetworkImage(
-                                "https://emrealtunbilek.com/wp-content/uploads/2016/10/apple-icon-72x72.png"),
-                            backgroundColor: Colors.purple,
-                            radius: 30,
-                            foregroundColor: Colors.orange,
-                          ),
-                          Text("Circle Avatar"),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              )
-            ),
-            IntrinsicHeight(
-                child:Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Expanded(
-                      child: Container(
-                        margin: EdgeInsets.all(4),
-                        color: Colors.red.shade200,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            //Image.asset("assets/images/icon.png"),
-                            //Image.network("http://hdqwalls.com/wallpapers/small-memory-lp.jpg"),
-                            FadeInImage.assetNetwork(placeholder: "assets/images/loading.gif", image: "http://hdqwalls.com/wallpapers/small-memory-lp.jpg",),
-                            Text("Fade In Image"),
-                          ],
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(4),
+                    color: Colors.red.shade200,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        CircleAvatar(
+                          backgroundImage: NetworkImage(
+                              "https://emrealtunbilek.com/wp-content/uploads/2016/10/apple-icon-72x72.png"),
+                          backgroundColor: Colors.purple,
+                          radius: 30,
+                          foregroundColor: Colors.orange,
                         ),
-                      ),
+                        Text("Circle Avatar"),
+                      ],
                     ),
-                    
-                  ],
-                )
-            ),
+                  ),
+                ),
+              ],
+            )),
+            IntrinsicHeight(
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(4),
+                    color: Colors.red.shade200,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        //Image.asset("assets/images/icon.png"),
+                        //Image.network("http://hdqwalls.com/wallpapers/small-memory-lp.jpg"),
+                        FadeInImage.assetNetwork(
+                          placeholder: "assets/images/loading.gif",
+                          image:
+                              "http://hdqwalls.com/wallpapers/small-memory-lp.jpg",
+                        ),
+                        Text("Fade In Image"),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(4),
+                    color: Colors.red.shade200,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        FlutterLogo(
+                          size: 60,
+                          colors: Colors.orange,
+                          style: FlutterLogoStyle.horizontal,
+                          textColor: Colors.black,
+                        ),
+                        Text("Flutter Logo"),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(4),
+                    color: Colors.red.shade200,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Expanded(
+                          child: Placeholder(
+                            color: Colors.red,
+                            strokeWidth: 2.0,
+                          ),
+                        ),
+                        Text("Placeholder Widget"),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            )),
           ],
         ),
       ),
