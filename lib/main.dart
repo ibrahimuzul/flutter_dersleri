25b238fc128766ae3c5c0a4fe0c495c6a3ed8d27
iuzul <iuzul@IBRAHIMUZUL>
2020-09-22 10:46:28 +0300

    64. Listeler için Card ListTile Divider Öğrenelim

--- lib/main.dart
+++ lib/main.dart
@@ -1,99 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dersleri/ui/liste_dersleri.dart';
 
 void main() {
-  runApp(new MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      title: "MaterialApp Title",
-      home: MyHomePage(title: "My Home Page"),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  final String title;
-  int sayac = 0;
-
-  MyHomePage({this.title}) {
-    debugPrint("MyhomePage Statefull widget constructor çalıştırıldı");
-  }
-
-  @override
-  State<StatefulWidget> createState() {
-    debugPrint("MyhomePage Statefull widget createState çalıştırıldı");
-    return _MyHomePageState();
-  }
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  _MyHomePageState() {
-    debugPrint("MyHomePageState State  constructor çalıştırıldı");
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    debugPrint("MyHomePageState build metodu tetkiklendi");
-    return Scaffold(
+  runApp(MaterialApp(
+    title: "Flutter Dersleri",
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primarySwatch: Colors.orange,
+    ),
+    home: Scaffold(
       appBar: AppBar(
-        title: Text("${widget.title}"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _sayacDegeriniArttir();
-            debugPrint("sayac değeri arttırıldı set state çağırıldı");
-          });
-        },
-        child: Icon(Icons.add),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            RaisedButton(
-              child: Text("Arttır"),
-              color: Colors.green,
-              onPressed: () {
-                _sayacDegeriniArttir();
-              },
-            ),
-            Text("${widget.sayac}",
-                style: Theme.of(context).textTheme.display2.copyWith(
-                  color:widget.sayac<0 ? Colors.red : Colors.green
-                )),
-            RaisedButton(
-              child: Text("Arttır"),
-              color: Colors.red,
-              onPressed: () {
-                _sayacDegeriniAzalt();
-              },
-            ),
-          ],
+        title: Text(
+          "Liste Dersleri",
+          style: TextStyle(
+            color: Colors.white,
+          ),
         ),
       ),
-    );
-  }
-
-  void _sayacDegeriniArttir() {
-    setState(() {
-      widget.sayac++;
-      debugPrint(widget.sayac.toString());
-    });
-
-  }
-
-  void _sayacDegeriniAzalt() {
-    setState(() {
-      widget.sayac--;
-      debugPrint(widget.sayac.toString());
-    });
-
-  }
+      body: ListeKonuAnlatimi(),
+    ),
+  ));
 }
---
+++ lib/ui/varsayilan_uygulama.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class VarsayilanUygulama extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      title: "MaterialApp Title",
+      home: MyHomePage(title: "My Home Page"),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  int sayac = 0;
+
+  MyHomePage({this.title}) {
+    debugPrint("MyhomePage Statefull widget constructor çalıştırıldı");
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    debugPrint("MyhomePage Statefull widget createState çalıştırıldı");
+    return _MyHomePageState();
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  _MyHomePageState() {
+    debugPrint("MyHomePageState State  constructor çalıştırıldı");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint("MyHomePageState build metodu tetkiklendi");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("${widget.title}"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _sayacDegeriniArttir();
+            debugPrint("sayac değeri arttırıldı set state çağırıldı");
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RaisedButton(
+              child: Text("Arttır"),
+              color: Colors.green,
+              onPressed: () {
+                _sayacDegeriniArttir();
+              },
+            ),
+            Text("${widget.sayac}",
+                style: Theme.of(context).textTheme.display2.copyWith(
+                    color:widget.sayac<0 ? Colors.red : Colors.green
+                )),
+            RaisedButton(
+              child: Text("Arttır"),
+              color: Colors.red,
+              onPressed: () {
+                _sayacDegeriniAzalt();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _sayacDegeriniArttir() {
+    setState(() {
+      widget.sayac++;
+      debugPrint(widget.sayac.toString());
+    });
+
+  }
+
+  void _sayacDegeriniAzalt() {
+    setState(() {
+      widget.sayac--;
+      debugPrint(widget.sayac.toString());
+    });
+
+  }
+}
