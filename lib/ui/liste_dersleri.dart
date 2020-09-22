6aa5a77638ccff2bd8c199fbb65ece239ac789a7
iuzul <iuzul@IBRAHIMUZUL>
2020-09-22 10:55:56 +0300

    65. Listview Temel Kullanımı

--- lib/ui/liste_dersleri.dart
+++ lib/ui/liste_dersleri.dart
@@ -3,206 +3,301 @@ import 'package:flutter/material.dart';
 class ListeKonuAnlatimi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          child: Card(
-            color: Colors.teal.shade100,
-            margin: EdgeInsets.all(10),
-            elevation: 10,
-            child: ListTile(
-              leading: CircleAvatar(
-                child: Icon(Icons.android),
-                radius: 12,
-              ),
-              title: Text("Liste eleman başlık"),
-              subtitle: Text("Liste elemanı alt başlık"),
-              trailing: Icon(Icons.add_circle),
-            ),
-          ),
-        ),
-        Divider(color: Colors.orange, height: 0,indent: 20,),
+    return  ListView(
+      children: [
+        Column(
+          children: <Widget>[
 
-        Container(
-          child: Card(
-            color: Colors.teal.shade100,
-            margin: EdgeInsets.all(10),
-            elevation: 10,
-            child: ListTile(
-              leading: CircleAvatar(
-                child: Icon(Icons.android),
-                radius: 12,
+            Container(
+              child: Card(
+                color: Colors.teal.shade100,
+                margin: EdgeInsets.all(10),
+                elevation: 10,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Icon(Icons.android),
+                    radius: 12,
+                  ),
+                  title: Text("Liste eleman başlık"),
+                  subtitle: Text("Liste elemanı alt başlık"),
+                  trailing: Icon(Icons.add_circle),
+                ),
               ),
-              title: Text("Liste eleman başlık"),
-              subtitle: Text("Liste elemanı alt başlık"),
-              trailing: Icon(Icons.add_circle),
             ),
-          ),
-        ),
-        Divider(color: Colors.orange, height: 0,indent: 20,),
+            Divider(color: Colors.orange, height: 0,indent: 20,),
 
-        Container(
-          child: Card(
-            color: Colors.teal.shade100,
-            margin: EdgeInsets.all(10),
-            elevation: 10,
-            child: ListTile(
-              leading: CircleAvatar(
-                child: Icon(Icons.android),
-                radius: 12,
-              ),
-              title: Text("Liste eleman başlık"),
-              subtitle: Text("Liste elemanı alt başlık"),
-              trailing: Icon(Icons.add_circle),
-            ),
-          ),
-        ),
-        Divider(color: Colors.orange, height: 0,indent: 20,),
 
-        Container(
-          child: Card(
-            color: Colors.teal.shade100,
-            margin: EdgeInsets.all(10),
-            elevation: 10,
-            child: ListTile(
-              leading: CircleAvatar(
-                child: Icon(Icons.android),
-                radius: 12,
-              ),
-              title: Text("Liste eleman başlık"),
-              subtitle: Text("Liste elemanı alt başlık"),
-              trailing: Icon(Icons.add_circle),
-            ),
-          ),
+          ],
         ),
-        Divider(color: Colors.orange, height: 0,indent: 20,),
+        Column(
+          children: <Widget>[
 
-        Container(
-          child: Card(
-            color: Colors.teal.shade100,
-            margin: EdgeInsets.all(10),
-            elevation: 10,
-            child: ListTile(
-              leading: CircleAvatar(
-                child: Icon(Icons.android),
-                radius: 12,
+            Container(
+              child: Card(
+                color: Colors.teal.shade100,
+                margin: EdgeInsets.all(10),
+                elevation: 10,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Icon(Icons.android),
+                    radius: 12,
+                  ),
+                  title: Text("Liste eleman başlık"),
+                  subtitle: Text("Liste elemanı alt başlık"),
+                  trailing: Icon(Icons.add_circle),
+                ),
               ),
-              title: Text("Liste eleman başlık"),
-              subtitle: Text("Liste elemanı alt başlık"),
-              trailing: Icon(Icons.add_circle),
             ),
-          ),
-        ),
-        Divider(color: Colors.orange, height: 0,indent: 20,),
+            Divider(color: Colors.orange, height: 0,indent: 20,),
 
-        Container(
-          child: Card(
-            color: Colors.teal.shade100,
-            margin: EdgeInsets.all(10),
-            elevation: 10,
-            child: ListTile(
-              leading: CircleAvatar(
-                child: Icon(Icons.android),
-                radius: 12,
-              ),
-              title: Text("Liste eleman başlık"),
-              subtitle: Text("Liste elemanı alt başlık"),
-              trailing: Icon(Icons.add_circle),
-            ),
-          ),
-        ),
-        Divider(color: Colors.orange, height: 0,indent: 20,),
 
-        Container(
-          child: Card(
-            color: Colors.teal.shade100,
-            margin: EdgeInsets.all(10),
-            elevation: 10,
-            child: ListTile(
-              leading: CircleAvatar(
-                child: Icon(Icons.android),
-                radius: 12,
-              ),
-              title: Text("Liste eleman başlık"),
-              subtitle: Text("Liste elemanı alt başlık"),
-              trailing: Icon(Icons.add_circle),
-            ),
-          ),
+          ],
         ),
-        Divider(color: Colors.orange, height: 0,indent: 20,),
+        Column(
+          children: <Widget>[
 
-        Container(
-          child: Card(
-            color: Colors.teal.shade100,
-            margin: EdgeInsets.all(10),
-            elevation: 10,
-            child: ListTile(
-              leading: CircleAvatar(
-                child: Icon(Icons.android),
-                radius: 12,
+            Container(
+              child: Card(
+                color: Colors.teal.shade100,
+                margin: EdgeInsets.all(10),
+                elevation: 10,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Icon(Icons.android),
+                    radius: 12,
+                  ),
+                  title: Text("Liste eleman başlık"),
+                  subtitle: Text("Liste elemanı alt başlık"),
+                  trailing: Icon(Icons.add_circle),
+                ),
               ),
-              title: Text("Liste eleman başlık"),
-              subtitle: Text("Liste elemanı alt başlık"),
-              trailing: Icon(Icons.add_circle),
             ),
-          ),
-        ),
-        Divider(color: Colors.orange, height: 0,indent: 20,),
+            Divider(color: Colors.orange, height: 0,indent: 20,),
 
-        Container(
-          child: Card(
-            color: Colors.teal.shade100,
-            margin: EdgeInsets.all(10),
-            elevation: 10,
-            child: ListTile(
-              leading: CircleAvatar(
-                child: Icon(Icons.android),
-                radius: 12,
-              ),
-              title: Text("Liste eleman başlık"),
-              subtitle: Text("Liste elemanı alt başlık"),
-              trailing: Icon(Icons.add_circle),
-            ),
-          ),
-        ),
-        Divider(color: Colors.orange, height: 0,indent: 20,),
 
-        Container(
-          child: Card(
-            color: Colors.teal.shade100,
-            margin: EdgeInsets.all(10),
-            elevation: 10,
-            child: ListTile(
-              leading: CircleAvatar(
-                child: Icon(Icons.android),
-                radius: 12,
-              ),
-              title: Text("Liste eleman başlık"),
-              subtitle: Text("Liste elemanı alt başlık"),
-              trailing: Icon(Icons.add_circle),
-            ),
-          ),
+          ],
         ),
-        Divider(color: Colors.orange, height: 0,indent: 20,),
+        Column(
+          children: <Widget>[
 
-        Container(
-          child: Card(
-            color: Colors.teal.shade100,
-            margin: EdgeInsets.all(10),
-            elevation: 10,
-            child: ListTile(
-              leading: CircleAvatar(
-                child: Icon(Icons.android),
-                radius: 12,
+            Container(
+              child: Card(
+                color: Colors.teal.shade100,
+                margin: EdgeInsets.all(10),
+                elevation: 10,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Icon(Icons.android),
+                    radius: 12,
+                  ),
+                  title: Text("Liste eleman başlık"),
+                  subtitle: Text("Liste elemanı alt başlık"),
+                  trailing: Icon(Icons.add_circle),
+                ),
               ),
-              title: Text("Liste eleman başlık"),
-              subtitle: Text("Liste elemanı alt başlık"),
-              trailing: Icon(Icons.add_circle),
             ),
-          ),
+            Divider(color: Colors.orange, height: 0,indent: 20,),
+
+
+          ],
         ),
-        Divider(color: Colors.orange, height: 0,indent: 20,),
+        Column(
+          children: <Widget>[
+
+            Container(
+              child: Card(
+                color: Colors.teal.shade100,
+                margin: EdgeInsets.all(10),
+                elevation: 10,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Icon(Icons.android),
+                    radius: 12,
+                  ),
+                  title: Text("Liste eleman başlık"),
+                  subtitle: Text("Liste elemanı alt başlık"),
+                  trailing: Icon(Icons.add_circle),
+                ),
+              ),
+            ),
+            Divider(color: Colors.orange, height: 0,indent: 20,),
+
+
+          ],
+        ),
+        Column(
+          children: <Widget>[
+
+            Container(
+              child: Card(
+                color: Colors.teal.shade100,
+                margin: EdgeInsets.all(10),
+                elevation: 10,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Icon(Icons.android),
+                    radius: 12,
+                  ),
+                  title: Text("Liste eleman başlık"),
+                  subtitle: Text("Liste elemanı alt başlık"),
+                  trailing: Icon(Icons.add_circle),
+                ),
+              ),
+            ),
+            Divider(color: Colors.orange, height: 0,indent: 20,),
+
+
+          ],
+        ),
+        Column(
+          children: <Widget>[
+
+            Container(
+              child: Card(
+                color: Colors.teal.shade100,
+                margin: EdgeInsets.all(10),
+                elevation: 10,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Icon(Icons.android),
+                    radius: 12,
+                  ),
+                  title: Text("Liste eleman başlık"),
+                  subtitle: Text("Liste elemanı alt başlık"),
+                  trailing: Icon(Icons.add_circle),
+                ),
+              ),
+            ),
+            Divider(color: Colors.orange, height: 0,indent: 20,),
+
+
+          ],
+        ),
+        Column(
+          children: <Widget>[
+
+            Container(
+              child: Card(
+                color: Colors.teal.shade100,
+                margin: EdgeInsets.all(10),
+                elevation: 10,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Icon(Icons.android),
+                    radius: 12,
+                  ),
+                  title: Text("Liste eleman başlık"),
+                  subtitle: Text("Liste elemanı alt başlık"),
+                  trailing: Icon(Icons.add_circle),
+                ),
+              ),
+            ),
+            Divider(color: Colors.orange, height: 0,indent: 20,),
+
+
+          ],
+        ),
+        Column(
+          children: <Widget>[
+
+            Container(
+              child: Card(
+                color: Colors.teal.shade100,
+                margin: EdgeInsets.all(10),
+                elevation: 10,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Icon(Icons.android),
+                    radius: 12,
+                  ),
+                  title: Text("Liste eleman başlık"),
+                  subtitle: Text("Liste elemanı alt başlık"),
+                  trailing: Icon(Icons.add_circle),
+                ),
+              ),
+            ),
+            Divider(color: Colors.orange, height: 0,indent: 20,),
+
+
+          ],
+        ),
+        Column(
+          children: <Widget>[
+
+            Container(
+              child: Card(
+                color: Colors.teal.shade100,
+                margin: EdgeInsets.all(10),
+                elevation: 10,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Icon(Icons.android),
+                    radius: 12,
+                  ),
+                  title: Text("Liste eleman başlık"),
+                  subtitle: Text("Liste elemanı alt başlık"),
+                  trailing: Icon(Icons.add_circle),
+                ),
+              ),
+            ),
+            Divider(color: Colors.orange, height: 0,indent: 20,),
+
+
+          ],
+        ),
+        Column(
+          children: <Widget>[
+
+            Container(
+              child: Card(
+                color: Colors.teal.shade100,
+                margin: EdgeInsets.all(10),
+                elevation: 10,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Icon(Icons.android),
+                    radius: 12,
+                  ),
+                  title: Text("Liste eleman başlık"),
+                  subtitle: Text("Liste elemanı alt başlık"),
+                  trailing: Icon(Icons.add_circle),
+                ),
+              ),
+            ),
+            Divider(color: Colors.orange, height: 0,indent: 20,),
+
+
+          ],
+        ),
+        Column(
+          children: <Widget>[
+
+            Container(
+              child: Card(
+                color: Colors.teal.shade100,
+                margin: EdgeInsets.all(10),
+                elevation: 10,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Icon(Icons.android),
+                    radius: 12,
+                  ),
+                  title: Text("Liste eleman başlık"),
+                  subtitle: Text("Liste elemanı alt başlık"),
+                  trailing: Icon(Icons.add_circle),
+                ),
+              ),
+            ),
+            Divider(color: Colors.orange, height: 0,indent: 20,),
+
+
+          ],
+        ),
+
       ],
     );
+
+
+
   }
 }
