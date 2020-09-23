428abddc486d6ec5034f5795e692994efd9515ce
iuzul <iuzul@IBRAHIMUZUL>
2020-09-23 17:08:22 +0300

    79. PushReplacement Kullanımı ve İsimlendirilmiş Rota Tanımlama

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,24 @@
+
 import 'package:flutter/material.dart';
 import 'ui/custom_scroll_ve_slivers.dart';
 import 'ui/navigasyon_islemleri.dart';
 import 'ui/grid_view_kullanimi.dart';
 
+
 void main() {
   runApp(MaterialApp(
     title: "Flutter Dersleri",
+    routes: {
+      '/'      : (context) => NavigasyonIslemleri(),
+      '/CPage' : (context) => CSayfasi(),
+      '/DPage' : (context) => DSayfasi(),
+      '/GPage' : (context) => GSayfasi(),
+      '/FPage' : (context) => FSayfasi(),
+    },
     debugShowCheckedModeBanner: false,
     theme: ThemeData(
       primarySwatch: Colors.orange,
     ),
-    home: NavigasyonIslemleri(),
+    //home: NavigasyonIslemleri(),
   ));
 }
