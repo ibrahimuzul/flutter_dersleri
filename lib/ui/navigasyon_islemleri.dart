e8cdd13962a48c42c3666c5e932f4bdffee027c0
iuzul <iuzul@IBRAHIMUZUL>
2020-09-23 15:30:04 +0300

    77. Bir önceki Sayfaya Geri Dönmek Navigator pop Kullanımı

--- lib/ui/navigasyon_islemleri.dart
+++ lib/ui/navigasyon_islemleri.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class NavigasyonIslemleri extends StatelessWidget {
+  String baslik = "B SAYFASI";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,12 +20,34 @@ class NavigasyonIslemleri extends StatelessWidget {
                   child: Text("A Sayfasına Git"),
                   color: Colors.blue,
                   onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => ASayfasi()),);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ASayfasi()),
+                    );
+                  },
+                ),
+                RaisedButton(
+                  child: Text("B Sayfasına Git ve Veri Gönder"),
+                  color: Colors.red,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => BSayfasi(baslik)),
+                    );
+                  },
+                ),
+                RaisedButton(
+                  child: Text("C Sayfasına Git ve Geri Gel"),
+                  color: Colors.purple,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => CSayfasi()),
+                    );
                   },
                 ),
               ],
-            )
-        ));
+            )));
   }
 }
 
@@ -41,10 +65,76 @@ class ASayfasi extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text("A SAYFASI",
-                  style: TextStyle(color: Colors.red, fontSize: 24),)
+                Text(
+                  "A SAYFASI",
+                  style: TextStyle(color: Colors.red, fontSize: 24),
+                )
               ],
-            )
-        ));
+            )));
+  }
+}
+
+class BSayfasi extends StatelessWidget {
+  String gelenBaslikVerisi;
+
+  BSayfasi(this.gelenBaslikVerisi);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            gelenBaslikVerisi,
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  gelenBaslikVerisi,
+                  style: TextStyle(color: Colors.red, fontSize: 24),
+                )
+              ],
+            )));
+  }
+}
+
+class CSayfasi extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "C Sayfası",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "C SAYFASI",
+                  style: TextStyle(color: Colors.red, fontSize: 24),
+                ),
+                RaisedButton(
+                  color: Colors.purple,
+                  child: Text("Geri Dön"),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+
+                RaisedButton(
+                  color: Colors.purple,
+                  child: Text("A Sayfasına Git"),
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => ASayfasi()));
+                  },
+                ),
+              ],
+            )));
   }
 }
