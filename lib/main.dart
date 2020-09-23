072619d6a42a39783352cef4a9c8d76b025f6706
iuzul <iuzul@IBRAHIMUZUL>
2020-09-23 14:21:34 +0300

    76. Farklı Bir Sayfaya Geçiş Navigator push Kullanımı

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dersleri/ui/etkin_liste_ornek.dart';
-
 import 'ui/custom_scroll_ve_slivers.dart';
+import 'ui/navigasyon_islemleri.dart';
 import 'ui/grid_view_kullanimi.dart';
-import 'ui/row_column_grid.dart';
 
 void main() {
-
   runApp(MaterialApp(
     title: "Flutter Dersleri",
     debugShowCheckedModeBanner: false,
     theme: ThemeData(
       primarySwatch: Colors.orange,
     ),
-    home: Scaffold(
-      /*appBar: AppBar(
-        title: Text(
-          "Liste Dersleri",
-          style: TextStyle(
-            color: Colors.white,
-          ),
-        ),
-      ),*/
-      body: CollapsableToolbarOrnek(),
-    ),
+    home: NavigasyonIslemleri(),
   ));
 }
